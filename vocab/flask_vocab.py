f7b0fa176bba7c319fe941c79015c8c5b3a1a9ba
Jeske Glenn <jeskeg@uoregon.edu>
2017-10-13 17:39:14 -0700

    Removed submit button and AJAXified the program

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -26,7 +26,7 @@ app.secret_key = CONFIG.SECRET_KEY  # Should allow using session variables
 # or else read it from the file on each request/responce cycle,
 # neither of which would be suitable for responding keystroke by keystroke.
 
-WORDS = Vocab(CONFIG.vocab)
+WORDS = Vocab(CONFIG.VOCAB)
 
 ###
 # Pages
@@ -72,48 +72,8 @@ def success():
 #######################
 
 
-@app.route("/_check", methods=["POST"])
-def check():
-    """
-    User has submitted the form with a word ('attempt')
-    that should be formed from the jumble and on the
-    vocabulary list.  We respond depending on whether
-    the word is on the vocab list (therefore correctly spelled),
-    made only from the jumble letters, and not a word they
-    already found.
-    """
-    app.logger.debug("Entering check")
 
-    # The data we need, from form and from cookie
-    text = request.form["attempt"]
-    jumble = flask.session["jumble"]
-    matches = flask.session.get("matches", [])  # Default to empty list
 
-    # Is it good?
-    in_jumble = LetterBag(jumble).contains(text)
-    matched = WORDS.has(text)
-
-    # Respond appropriately
-    if matched and in_jumble and not (text in matches):
-        # Cool, they found a new word
-        matches.append(text)
-        flask.session["matches"] = matches
-    elif text in matches:
-        flask.flash("You already found {}".format(text))
-    elif not matched:
-        flask.flash("{} isn't in the list of words".format(text))
-    elif not in_jumble:
-        flask.flash(
-            '"{}" can\'t be made from the letters {}'.format(text, jumble))
-    else:
-        app.logger.debug("This case shouldn't happen!")
-        assert False  # Raises AssertionError
-
-        # Choose page:  Solved enough, or keep going?
-        if len(matches) >= flask.session["target_count"]:
-            return flask.redirect(url_for("success"))
-        else:
-            return flask.redirect(url_for("keep_going"))
 
 ###############
 # AJAX request handlers
@@ -121,14 +81,42 @@ def check():
 ###############
 
 
-@app.route("/_example")
-def example():
+@app.route("/_checkword")
+def checkword():
     """
-    Example ajax request handler
+    Checks to see if input string uses the letters in the scramble
+    and creates a word from the word list.
     """
     app.logger.debug("Got a JSON request")
-    rslt = {"key": "value"}
-    return jsonify(result=rslt)
+
+    text = flask.request.args.get("text", type=str)
+    jumble = flask.session["jumble"]
+    matches = flask.session.get("matches", [])  # Default to empty list
+
+    # Does text use the jumbled letters?
+    in_jumble = LetterBag(jumble).contains(text)
+    #Is text a word in the matches?
+    matched = WORDS.has(text)
+
+    # Respond appropriately
+    if matched and in_jumble and not (text in matches):
+        # New anagram found!
+        matches.append(text)
+        flask.session["matches"] = matches
+        # success = (len(matches) == flask.session.target_count)
+        rslt = {"matches": " ".join(matches)}  #, "success": success}
+        return flask.jsonify(result=rslt)
+
+    elif text in matches:
+        app.logger.debug("Already found this word")
+    elif not matched:
+        app.logger.debug("Word not in the matches")
+    elif not in_jumble:
+        app.logger.debug("Letters typed not in the jumble")
+    else:
+        app.logger.debug("This case shouldn't happen!")
+        assert False  # Raises AssertionError
+
 
 
 #################
